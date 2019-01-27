3b9c2d4202c27e900a1066077b255508064ce1ff
hgtruong <huygtruong@gmail.com>
2019-01-27 10:19:58 -0800

    - Added ESLint to use AirBnB style guide - Fixed formatting to meet AirBnB style

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+// eslint-disable-next-line no-unused-vars
 import List from './components/List.jsx';
 
+// eslint-disable-next-line no-unused-vars
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
+    this.state = {
+      items: [],
+    };
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/items', 
+      url: '/items',
       success: (data) => {
         this.setState({
-          items: data
-        })
+          items: data,
+        });
       },
       error: (err) => {
+        // eslint-disable-next-line no-console
         console.log('err', err);
-      }
+      },
     });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Item List</h1>
+        <List items={this.state.items}/>
+      </div>
+    );
   }
 }
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('app'));
