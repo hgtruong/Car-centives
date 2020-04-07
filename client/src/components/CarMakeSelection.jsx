a3749134f32f549af1632f314f640c5026cd6557
Huy Truong <ht@Huys-MacBook-Pro.local>
2020-04-06 19:33:52 -0700

    updated to use react hook API service calls

--- client/src/components/CarMakeSelection.jsx
+++ client/src/components/CarMakeSelection.jsx
@@ -2,7 +2,85 @@
 /* eslint-disable no-console */
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
-import {Select, InputLabel, MenuItem, FormControl, makeStyles} from '@material-ui/core';
+import {
+  Select, 
+  InputLabel, 
+  MenuItem, 
+  FormControl, 
+  makeStyles,
+  TextField
+} from '@material-ui/core';
+
+function CarMakeSelection(props) {
+  const classes = useStyles();
+  const make = useFormInput('');
+  const model = useFormInput('');
+  let [makes, makesAPICall] = useAPIService();
+
+  useEffect(() => {
+    makesAPICall('makes', 'GET');
+  }, []);
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="makeLabel">
+          Make
+        </InputLabel>
+        <Select {...make}>
+          <MenuItem value=""> 
+            <em> None </em> 
+          </MenuItem>
+          {makes.data.map((currentMake, key) => <MenuItem value={currentMake.make} key={key}>{ currentMake.make }</MenuItem>)}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+function useAPIService() {
+  const [data, setData]  = useState([]);
+  const [isLoading, setIsLoading]  = useState(false);
+  const [error, setError]  = useState(false);
+
+  function init() {
+    setData([]);
+    setIsLoading(false);
+    setError(false);
+  }
+
+  const serviceCall = async (url, method, payLoad) => {
+    init();
+    setIsLoading(true);
+
+    try {
+      const result = await axios({
+        method: `${method}`,
+        url: `${url}`,
+      });
+      setData(result.data);
+    } catch (error) {
+      console.log(`Error with ${method} API call for url: ${url}`);
+      setError(true);
+    }
+    setIsLoading(false);
+  }
+
+  return [{ data, isLoading, error}, serviceCall ];
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -13,31 +91,5 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
 }));
-
-function CarMakeSelection(props) {
-  const classes = useStyles();
-  const [makeValue, setMake] = useState('');
-
-  function handleMakeChange(event) {
-    setMake(event.target.value);
-  }
-
-  return (
-    <FormControl className={classes.formControl}>
-      <InputLabel shrink id="makesLabel">
-        Make*
-      </InputLabel>
-
-      <Select 
-        value={makeValue} 
-        onChange={handleMakeChange}>
-        <MenuItem value=""> 
-          <em> None </em> 
-        </MenuItem>
-        {props.makes.map((currentMake, key) => <MenuItem value={currentMake.make} key={key}>{ currentMake.make }</MenuItem>)}
-      </Select>
-    </FormControl>  
-  );
-}
 
 export { CarMakeSelection };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -17,31 +17,11 @@ class App extends React.Component {
     };
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  componentDidMount() {
-    this.retrieveMakes();
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  retrieveMakes() {
-    axios({
-      method: 'GET',
-      url: '/makes',
-    })
-      .then((result) => {
-        console.log('Retrieved all makes.', result.data);
-        this.setState({ makes: result.data });
-      })
-      .catch(() => {
-        console.log('Error retrieving makes.');
-      });
-  }
-
   render() {
     return (
       <div>
         <h1 id="welcome">Welcome</h1>
-        <CarMakeSelection makes={this.state.makes}/>
+        <CarMakeSelection />
       </div>
     );
   }
