0748e556b1ec6fa2d59abd7d7ee55fc8e352635c
Huy Truong <hgtruong@ucdavis.edu>
2021-01-30 16:24:48 -0800

    removed server and DB

--- server/CarList.js
+++
@@ -1,6 +0,0 @@
-const cars = {
-    "Acura": ["ILX", "TLX", "RLX", "RDX", "MDX", "NSX"],
-    "Honda": ["Clarity", "Accord", "Civic", "CR-V", "Odyssey", "Fit", "HR-V", "Pilot"],
-}
-
-module.exports = cars;
